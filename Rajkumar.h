00429a33e3462ca8d4d9ec3d50f628bb8658fe87
student <student@Achievers-iMac.local>
2017-06-19 14:51:32 +0530

    Task

---
+++ Rajkumar.h
@@ -0,0 +1,13 @@
+//
+//  Rajkumar.h
+//  Table
+//
+//  Created by student on 6/19/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rajkumar : NSObject
+
+@end
---
+++ Rajkumar.m
@@ -0,0 +1,13 @@
+//
+//  Rajkumar.m
+//  Table
+//
+//  Created by student on 6/19/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import "Rajkumar.h"
+
+@implementation Rajkumar
+
+@end
